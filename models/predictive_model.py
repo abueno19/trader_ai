3cf8242673070576dd74bcd248f861f75394bfe2
abueno19 <103993211+abueno19@users.noreply.github.com>
2025-03-02 12:04:48 +0000

    Modelo predictive

--- models/predictive_model.py
+++ models/predictive_model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import tensorflow as tf
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.optimizers import adam_v2 
+from tensorflow.python.keras.optimizers import adam_v2
 from tensorflow.python.keras.losses import BinaryCrossentropy
 from tensorflow.python.keras.layers.recurrent import LSTM
 from tensorflow.python.keras.layers import Dense
@@ -11,16 +11,16 @@ import numpy as np
 class PricePredictionModel(tf.keras.Model): # type: ignore
     def __init__(self, input_dim, hidden_dim, num_layers):
         super(PricePredictionModel, self).__init__()
-        # Crear capas LSTM
         self.lstm = Sequential([
             LSTM(hidden_dim, return_sequences=True, input_shape=(None, input_dim)),
+            *[LSTM(hidden_dim, return_sequences=True) for _ in range(num_layers - 1)],
             LSTM(hidden_dim),
             Dense(1, activation='sigmoid')
         ])
+        self.lstm.summary()
+
     def call(self, x):
-        
         return self.lstm(x)
-    
 
 class PredictiveModel:
     def __init__(self, data_path):
@@ -32,9 +32,7 @@ class PredictiveModel:
 
     def load_data(self):
         data = pd.read_csv(self.data_path, parse_dates=['Datetime'])
-        
         data['target'] = (data['close_price'] > data['open_price']).astype(int)
-        # data['target'] = 1 if data['open_price'] < data['close_price'] else 0
         data = data.dropna()
         return data
 
@@ -42,19 +40,9 @@ class PredictiveModel:
         X = data[self.features].values
         y = data['target'].values
 
-        # Verificar si hay valores NaN o infinitos en los datos originales
         if np.isnan(X).any() or np.isinf(X).any():
             raise ValueError("Los datos de entrada contienen valores NaN o infinitos antes de la normalización.")
 
-        # Normalización manual
-
-
-
-        # Verificar si hay valores NaN o infinitos después de la normalización
-        if np.isnan(X).any() or np.isinf(X).any():
-            raise ValueError("Los datos de entrada contienen valores NaN o infinitos después de la normalización.")
-
-        # Crear secuencias
         sequences = []
         targets = []
         for i in range(len(X) - sequence_length):
@@ -64,7 +52,7 @@ class PredictiveModel:
         X_sequences = tf.convert_to_tensor(sequences, dtype=tf.float32)
         y_sequences = tf.convert_to_tensor(targets, dtype=tf.float32)
 
-        # División manual del conjunto de datos
+# División manual del conjunto de datos
         dataset_size = len(X_sequences)
         test_size = int(0.2 * dataset_size)
         train_size = dataset_size - test_size
@@ -83,9 +71,9 @@ class PredictiveModel:
     def initialize_model(self):
         input_dim = len(self.features)
         hidden_dim = 64
-        num_layers = 2
+        num_layers = 10
         self.model = PricePredictionModel(input_dim, hidden_dim, num_layers)
-        self.model.compile(optimizer=self.optimizer, loss=self.criterion, metrics=['accuracy'])
+        self.model.compile(optimizer=self.optimizer, loss=self.criterion, metrics=['mae'])
 
     def train_model(self, X_train_tensor, y_train_tensor, epochs=100):
         if self.model is None:
@@ -95,8 +83,8 @@ class PredictiveModel:
     def evaluate_model(self, X_test_tensor, y_test_tensor):
         if self.model is None:
             raise ValueError("El modelo no ha sido inicializado.")
-        loss, accuracy = self.model.evaluate(X_test_tensor, y_test_tensor)
-        print(f'Accuracy: {accuracy:.4f} - Loss: {loss:.4f}')
+        loss, mae = self.model.evaluate(X_test_tensor, y_test_tensor)
+        print(f'MAE: {mae:.4f} - Loss: {loss:.4f}')
 
     def run(self):
         data = self.load_data()
